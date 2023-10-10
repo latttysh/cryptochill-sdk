6966e9c29502291dda50149da3524c466af551b3
7yon <semyon.latysh@gmail.com>
2023-10-10 17:11:04 +0300

    initial commit with base class

---
+++ src/base.ts
@@ -0,0 +1,50 @@
+import axios from "axios"
+import * as crypto from "crypto"
+import { Base64 } from "js-base64"
+
+type Config = {
+    apiUrl?: string
+    apiKey: string
+    profileId: string
+    apiSecret: string
+    callbackToken: string
+}
+
+export abstract class CryptochillBase {
+    private apiKey: string;
+    private apiUrl: string;
+    private profileId: string
+    private apiSecret: string
+    private callbackToken: string | ""
+
+    constructor(config: Config) {
+        this.apiKey = config.apiKey
+        this.apiUrl = config.apiUrl ? config.apiUrl : "https://api.cryptochill.com"
+        this.profileId = config.profileId
+        this.apiSecret = config.apiSecret
+        this.callbackToken = config.callbackToken
+    }
+
+    public encodeHmac(key: string, message: string): string {
+        return crypto.createHmac('sha256', key).update(message).digest('hex')
+    }
+
+    protected cryptochillApiRequest<T>(endpoint: string, payload: any, method = "GET"): Promise<T> {
+        const request_path = '/v1/' + endpoint + '/';
+        payload.request = request_path
+        payload.nonce = new Date().getTime()
+        const encoded_payload = JSON.stringify(payload)
+        const b64 = Base64.encode(encoded_payload)
+        const signature = this.encodeHmac(this.apiSecret, b64)
+
+        return axios({
+            method: method,
+            url: this.apiUrl + request_path,
+            headers: {
+                'X-CC-KEY': this.apiKey,
+                'X-CC-PAYLOAD': b64,
+                'X-CC-SIGNATURE': signature,
+            }
+        })
+    }
+}
