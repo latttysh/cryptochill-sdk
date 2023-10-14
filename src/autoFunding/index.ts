30e01970600ab398bdedf334b06488ddb7eb03de
7yon <semyon.latysh@gmail.com>
2023-10-14 20:53:57 +0300

    add auto funding

--- src/autoFunding/index.ts
+++ src/autoFunding/index.ts
@@ -3,6 +3,23 @@ import { CryptochillBase } from "src/base";
 const endpoint = "automatic-funding"
 
 export class AutoFunding extends CryptochillBase {
+    listAutoFunding = () => {
+        return this.cryptochillApiRequest(endpoint)
+    }
 
+    getAutoFunding = (id: string) => {
+        return this.cryptochillApiRequest(endpoint + `/${id}`)
+    }
 
+    createAutoFunding = (payload: any) => {
+        return this.cryptochillApiRequest(endpoint, payload, "POST")
+    }
+
+    updateAutoFunding = (payload: any) => {
+        return this.cryptochillApiRequest(endpoint, payload, "PUT")
+    }
+
+    deleteAutoFunding = (id: string) => {
+        return this.cryptochillApiRequest(endpoint + `/${id}`, null, "DELETE")
+    }
 }
