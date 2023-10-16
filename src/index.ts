9b4232b487723731a75be67efb7471eae4f0a9af
7yon <semyon.latysh@gmail.com>
2023-10-16 19:44:47 +0300

    add init to main class

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,30 @@
 import { Account } from "./account";
+import { AutoConversions } from "./autoConversions";
+import { AutoFunding } from "./autoFunding";
+import { AutomaticPayouts } from "./autoPayouts";
+import { Callbacks } from "./callbacks";
+import { Conversions } from "./conversions";
+import { ExchangeRates } from "./exchangeRates";
 import { Invoices } from "./invoices";
+import { Payouts } from "./payouts";
+import { Profiles } from "./profiles";
+import { Transactions } from "./transactions";
+import { Wallets } from "./wallets";
 
 
 export class Cryptochill {
     account: Account
     invoices: Invoices
+    autoConversions: AutoConversions
+    autoFunding: AutoFunding
+    autoPayouts: AutomaticPayouts
+    callbacks: Callbacks
+    conversions: Conversions
+    exchangeRates: ExchangeRates
+    payouts: Payouts
+    profiles: Profiles
+    transactions: Transactions
+    wallets: Wallets
 
     constructor(config: {
         apiKey: string;
@@ -15,6 +35,17 @@ export class Cryptochill {
     }) {
         this.account = new Account(config)
         this.invoices = new Invoices(config)
+        this.autoConversions = new AutoConversions(config)
+        this.autoFunding = new AutoFunding(config)
+        this.autoPayouts = new AutomaticPayouts(config)
+        this.callbacks = new Callbacks(config)
+        this.conversions = new Conversions(config)
+        this.exchangeRates = new ExchangeRates(config)
+        this.payouts = new Payouts(config)
+        this.profiles = new Profiles(config)
+        this.transactions = new Transactions(config)
+        this.wallets = new Wallets(config)
+
     }
 }
 
